e1e90bc56fff9371bcd71519603e539485a7e005
Prometheusxiao <iapetosee@163.com>
2019-09-12 15:00:14 +0800

    引入公共模块

--- system-address-server-001/src/main/java/com/system/function/task/AddressTask.java
+++ system-address-server-001/src/main/java/com/system/function/task/AddressTask.java
@@ -18,6 +18,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
+import java.util.logging.Handler;
 
 /**
  * @CreateTime: 2019-09-11 09:26
@@ -33,8 +34,9 @@ public class AddressTask {
     @Autowired
     private AddressBasisInfoMapper addressBasisInfoMapper;
 
-    @Scheduled(cron = "0 28 15 * * * ")
+    @Scheduled(cron = "0 58 9 * * * ")
     public void getAddress() throws InterruptedException {
+        System.out.println("获取国家统计局地址编码 - 开始");
         String year = "2018";
         String basicUrl = "http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/" + year+"/";
         Map<String, String> firstPage = AddressRequest.getCookie(basicUrl+ "index.html");
@@ -79,6 +81,7 @@ public class AddressTask {
             }
             saveAddressBasisInfo(addressBasisInfoSheng);
         }
+        System.out.println("获取国家统计局地址编码 - 结束");
     }
 
     public void saveAddressBasisInfo(AddressBasisInfo addressBasisInfo){
@@ -92,24 +95,26 @@ public class AddressTask {
 
     public List<AddressBasisInfo> sheng(List<AddressBasisInfo> data ,String year,String html){
         List<AddressBasisInfo> addressBasisInfos = new ArrayList<>();
-        Document parse = Jsoup.parse(html);
-        Elements trs = parse.getElementsByClass("provincetr");
-        for (Element tr : trs) {
-            Elements as = tr.getElementsByTag("a");
-            for (Element a : as) {
-                String href = a.attr("href");
-                if(href.contains(".html")){
-                    AddressBasisInfo addressBaseInfo = new AddressBasisInfo();
-                    addressBaseInfo.setAddressUrl(href);
-                    String code = href.replace(".html", "");
-                    String text = a.text();
-                    addressBaseInfo.setSuperCode(year);
-                    addressBaseInfo.setCode(code);
-                    addressBaseInfo.setAddressName(text);
-                    addressBaseInfo.setAddressVersion(year);
-                    addressBaseInfo.setStatus("1");
-                    addressBasisInfos.add(addressBaseInfo);
-                    data.add(addressBaseInfo);
+        if(StringUtils.isNotBlank(html)) {
+            Document parse = Jsoup.parse(html);
+            Elements trs = parse.getElementsByClass("provincetr");
+            for (Element tr : trs) {
+                Elements as = tr.getElementsByTag("a");
+                for (Element a : as) {
+                    String href = a.attr("href");
+                    if (href.contains(".html")) {
+                        AddressBasisInfo addressBaseInfo = new AddressBasisInfo();
+                        addressBaseInfo.setAddressUrl(href);
+                        String code = href.replace(".html", "");
+                        String text = a.text();
+                        addressBaseInfo.setSuperCode(year);
+                        addressBaseInfo.setCode(code);
+                        addressBaseInfo.setAddressName(text);
+                        addressBaseInfo.setAddressVersion(year);
+                        addressBaseInfo.setStatus("1");
+                        addressBasisInfos.add(addressBaseInfo);
+                        data.add(addressBaseInfo);
+                    }
                 }
             }
         }
@@ -118,104 +123,110 @@ public class AddressTask {
 
     public List<AddressBasisInfo> shi(List<AddressBasisInfo> data,String year,String superCode,String html){
         List<AddressBasisInfo> addressBasisInfos = new ArrayList<>();
-        Document parse = Jsoup.parse(html);
-        Elements citytrs = parse.getElementsByClass("citytr");
-        for (Element citytr : citytrs) {
-            Elements as = citytr.getElementsByTag("a");
-            Element a0 = as.get(0);
-            String text = a0.text();
-            String href = a0.attr("href");
-            Element a1 = as.get(1);
-            String name = a1.text();
-            AddressBasisInfo addressBaseInfo = new AddressBasisInfo();
-            addressBaseInfo.setAddressUrl(href);
-            addressBaseInfo.setCode(text);
-            addressBaseInfo.setSuperCode(superCode);
-            addressBaseInfo.setAddressName(name);
-            addressBaseInfo.setAddressVersion(year);
-            addressBaseInfo.setStatus("1");
-            addressBasisInfos.add(addressBaseInfo);
-            data.add(addressBaseInfo);
+        if(StringUtils.isNotBlank(html)) {
+            Document parse = Jsoup.parse(html);
+            Elements citytrs = parse.getElementsByClass("citytr");
+            for (Element citytr : citytrs) {
+                Elements as = citytr.getElementsByTag("a");
+                Element a0 = as.get(0);
+                String text = a0.text();
+                String href = a0.attr("href");
+                Element a1 = as.get(1);
+                String name = a1.text();
+                AddressBasisInfo addressBaseInfo = new AddressBasisInfo();
+                addressBaseInfo.setAddressUrl(href);
+                addressBaseInfo.setCode(text);
+                addressBaseInfo.setSuperCode(superCode);
+                addressBaseInfo.setAddressName(name);
+                addressBaseInfo.setAddressVersion(year);
+                addressBaseInfo.setStatus("1");
+                addressBasisInfos.add(addressBaseInfo);
+                data.add(addressBaseInfo);
+            }
         }
         return addressBasisInfos;
     }
 
     public List<AddressBasisInfo> xian(List<AddressBasisInfo> data,String year,String superCode,String html){
         List<AddressBasisInfo> addressBasisInfos = new ArrayList<>();
-        Document parse = Jsoup.parse(html);
-        Elements countytrs = parse.getElementsByClass("countytr");
-        for (Element countytr : countytrs) {
-            Elements as = countytr.getElementsByTag("a");
-            if(as!=null&&as.size()>0){
-                Element a0 = as.get(0);
-                String text = a0.text();
-                String href = a0.attr("href");
-                Element a1 = as.get(1);
-                String name = a1.text();
-                AddressBasisInfo addressBaseInfo = new AddressBasisInfo();
-                addressBaseInfo.setAddressUrl(href);
-                addressBaseInfo.setCode(text);
-                addressBaseInfo.setSuperCode(superCode);
-                addressBaseInfo.setAddressName(name);
-                addressBaseInfo.setAddressVersion(year);
-                addressBaseInfo.setStatus("1");
-                addressBasisInfos.add(addressBaseInfo);
-                data.add(addressBaseInfo);
-            } else {
-                Elements tds = countytr.getElementsByTag("td");
-                String code = tds.get(0).text();
-                String name = tds.get(1).text();
-                AddressBasisInfo addressBaseInfo = new AddressBasisInfo();
-                addressBaseInfo.setAddressUrl(null);
-                addressBaseInfo.setCode(code);
-                addressBaseInfo.setSuperCode(superCode);
-                addressBaseInfo.setAddressName(name);
-                addressBaseInfo.setAddressVersion(year);
-                addressBaseInfo.setStatus("1");
-                addressBasisInfos.add(addressBaseInfo);
-                data.add(addressBaseInfo);
-            }
+        if(StringUtils.isNotBlank(html)) {
+            Document parse = Jsoup.parse(html);
+            Elements countytrs = parse.getElementsByClass("countytr");
+            for (Element countytr : countytrs) {
+                Elements as = countytr.getElementsByTag("a");
+                if (as != null && as.size() > 0) {
+                    Element a0 = as.get(0);
+                    String text = a0.text();
+                    String href = a0.attr("href");
+                    Element a1 = as.get(1);
+                    String name = a1.text();
+                    AddressBasisInfo addressBaseInfo = new AddressBasisInfo();
+                    addressBaseInfo.setAddressUrl(href);
+                    addressBaseInfo.setCode(text);
+                    addressBaseInfo.setSuperCode(superCode);
+                    addressBaseInfo.setAddressName(name);
+                    addressBaseInfo.setAddressVersion(year);
+                    addressBaseInfo.setStatus("1");
+                    addressBasisInfos.add(addressBaseInfo);
+                    data.add(addressBaseInfo);
+                } else {
+                    Elements tds = countytr.getElementsByTag("td");
+                    String code = tds.get(0).text();
+                    String name = tds.get(1).text();
+                    AddressBasisInfo addressBaseInfo = new AddressBasisInfo();
+                    addressBaseInfo.setAddressUrl(null);
+                    addressBaseInfo.setCode(code);
+                    addressBaseInfo.setSuperCode(superCode);
+                    addressBaseInfo.setAddressName(name);
+                    addressBaseInfo.setAddressVersion(year);
+                    addressBaseInfo.setStatus("1");
+                    addressBasisInfos.add(addressBaseInfo);
+                    data.add(addressBaseInfo);
+                }
 
+            }
         }
         return addressBasisInfos;
     }
 
     public List<AddressBasisInfo> jieZhen(List<AddressBasisInfo> data,String year,String superCode,String html){
         List<AddressBasisInfo> addressBasisInfos = new ArrayList<>();
-        Document parse = Jsoup.parse(html);
-        Elements towntrs = parse.getElementsByClass("towntr");
-        for (Element towntr : towntrs) {
-            Elements as = towntr.getElementsByTag("a");
-            if(as!=null&&as.size()>0){
-                Element a0 = as.get(0);
-                String text = a0.text();
-                String href = a0.attr("href");
-                Element a1 = as.get(1);
-                String name = a1.text();
-                AddressBasisInfo addressBaseInfo = new AddressBasisInfo();
-                addressBaseInfo.setAddressUrl(href);
-                addressBaseInfo.setCode(text);
-                addressBaseInfo.setSuperCode(superCode);
-                addressBaseInfo.setAddressName(name);
-                addressBaseInfo.setAddressVersion(year);
-                addressBaseInfo.setStatus("1");
-                addressBasisInfos.add(addressBaseInfo);
-                data.add(addressBaseInfo);
-            } else {
-                Elements tds = towntr.getElementsByTag("td");
-                String code = tds.get(0).text();
-                String name = tds.get(1).text();
-                AddressBasisInfo addressBaseInfo = new AddressBasisInfo();
-                addressBaseInfo.setAddressUrl(null);
-                addressBaseInfo.setCode(code);
-                addressBaseInfo.setSuperCode(superCode);
-                addressBaseInfo.setAddressName(name);
-                addressBaseInfo.setAddressVersion(year);
-                addressBaseInfo.setStatus("1");
-                addressBasisInfos.add(addressBaseInfo);
-                data.add(addressBaseInfo);
-            }
+        if(StringUtils.isNotBlank(html)){
+            Document parse = Jsoup.parse(html);
+            Elements towntrs = parse.getElementsByClass("towntr");
+            for (Element towntr : towntrs) {
+                Elements as = towntr.getElementsByTag("a");
+                if(as!=null&&as.size()>0){
+                    Element a0 = as.get(0);
+                    String text = a0.text();
+                    String href = a0.attr("href");
+                    Element a1 = as.get(1);
+                    String name = a1.text();
+                    AddressBasisInfo addressBaseInfo = new AddressBasisInfo();
+                    addressBaseInfo.setAddressUrl(href);
+                    addressBaseInfo.setCode(text);
+                    addressBaseInfo.setSuperCode(superCode);
+                    addressBaseInfo.setAddressName(name);
+                    addressBaseInfo.setAddressVersion(year);
+                    addressBaseInfo.setStatus("1");
+                    addressBasisInfos.add(addressBaseInfo);
+                    data.add(addressBaseInfo);
+                } else {
+                    Elements tds = towntr.getElementsByTag("td");
+                    String code = tds.get(0).text();
+                    String name = tds.get(1).text();
+                    AddressBasisInfo addressBaseInfo = new AddressBasisInfo();
+                    addressBaseInfo.setAddressUrl(null);
+                    addressBaseInfo.setCode(code);
+                    addressBaseInfo.setSuperCode(superCode);
+                    addressBaseInfo.setAddressName(name);
+                    addressBaseInfo.setAddressVersion(year);
+                    addressBaseInfo.setStatus("1");
+                    addressBasisInfos.add(addressBaseInfo);
+                    data.add(addressBaseInfo);
+                }
 
+            }
         }
         return addressBasisInfos;
     }
